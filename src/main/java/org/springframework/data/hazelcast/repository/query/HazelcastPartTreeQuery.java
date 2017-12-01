d5a70cf9f68c7268384d3f5c399d55de4e1cb631
Marius Jakobsen <marius@bokbasen.no>
2017-12-01 15:30:07 +0100

    BB-472 Updated to hazelcast 3.9.1, spring 4.3.13.RELEASE, spring-data 1.9.9.RELEASE

--- src/main/java/org/springframework/data/hazelcast/repository/query/HazelcastPartTreeQuery.java
+++ src/main/java/org/springframework/data/hazelcast/repository/query/HazelcastPartTreeQuery.java
@@ -68,7 +68,7 @@ public class HazelcastPartTreeQuery extends KeyValuePartTreeQuery {
 	 * </P>
 	 *
 	 * @param queryMethod Method defined in {@code HazelcastRepository}
-	 * @param evalulationContextProvider Not used
+	 * @param evaluationContextProvider Not used
 	 * @param keyValueOperations Interface to Hazelcast
 	 * @param queryCreator Not used
 	 */
@@ -302,7 +302,7 @@ public class HazelcastPartTreeQuery extends KeyValuePartTreeQuery {
 	 * </LI>
 	 * </OL>
 	 *
-	 * @param parameters Possibly empty
+	 * @param originalParameters Possibly empty
 	 * @param partTree Query tree to traverse
 	 * @return Paremeters in correct order
 	 */
